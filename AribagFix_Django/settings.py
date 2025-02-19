c8ec735a00e66e9eca6788741c9f1355a5641781
magnacin <gilberto.gonzalez.tovar@gmail.com>
2025-02-19 17:20:53 -0600

    Prueba con whitenoise y debug false 5

--- AribagFix_Django/settings.py
+++ AribagFix_Django/settings.py
@@ -130,10 +130,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if DEBUG = False:
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
