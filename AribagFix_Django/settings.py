6c9d7ee450e4441c455c956d0a964194606bba6a
magnacin <gilberto.gonzalez.tovar@gmail.com>
2025-02-20 13:23:06 -0600

    Cambio a variables de entorno

--- AribagFix_Django/settings.py
+++ AribagFix_Django/settings.py
@@ -14,14 +14,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # DJANGO_AIRBAGFIX_SK
-SECRET_KEY = os.getenv("SECRET_KEY", default = "131605iteso#") # Obtiene las variables de entorno que estan en otro lado. en este
+SECRET_KEY = os.getenv("SECRET_KEY", default = '') # Obtiene las variables de entorno que estan en otro lado. en este
 # caso la pondre directamente fija aqui 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # Para cuando trabajamos de manera local es True
 
-ALLOWED_HOSTS = ['www.airbagfix.mx','airbagfix2025.herokuapp.com',
-                 'airbagfix.mx','localhost', '127.0.0.1']#,'airbagfix2025.onrender.com'] # Que sitios web se permite acceder
+ALLOWED_HOSTS = ['www.airbagfix.mx',
+                 'airbagfix.mx','localhost', '127.0.0.1','airbagfix2025.onrender.com'] # Que sitios web se permite acceder
 
 
 # Application definition
@@ -79,10 +79,6 @@ DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL)
 }
 
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -117,11 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
 # Directorio donde Django guardará los archivos estáticos cuando se ejecute collectstatic
@@ -135,38 +126,19 @@ STATICFILES_DIRS = [
 # Configurar WhiteNoise para servir archivos estáticos en producción
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # Configuracion de Django para enviar mail
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtpout.secureserver.net' # Servidor de salida del proveedor del dominio
-EMAIL_PORT = 587 # email port Go Daddy
-EMAIL_HOST_USER = 'info@airbagfix.mx' # direccion a donde se enviaran los correos.
-EMAIL_HOST_PASSWORD = 'Wattolandia16954!' # NO ES RECOMENDABLE PONER EL PWD AQUI por que esto se subira a internet y quedara vulnerable
-# Leer ENVIROMENTAL VARIABLES
+EMAIL_HOST = 'smtpout.secureserver.net'  # Servidor SMTP
+EMAIL_PORT = 587  # Puerto SMTP
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')  # Tomar de variables de entorno
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')  # Tomar de variables de entorno
 EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False # Dependiendo del sistema se utiliza TLS o SSL
 
-"""
-
-# La siguiente version es para probar el sistema de mensajes desde el Local Host:
-# El siguiente comando es necesario ponerlo en la terminal para arrancar el "servidor" de correo local
-# python -m smtpd -n -c DebuggingServer localhost:1025
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-#EMAIL_USE_SSL = False # Dependiendo del sistema se utiliza TLS o SSL"""
 
 if os.getcwd() == '/app':
     DEBUG = False # Si estamos en vivo el directorio sera este y DEBUG es False
